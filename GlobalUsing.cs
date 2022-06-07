f1a8314fd67ec73fe7507f3713096d49042692f6
Igor Couto <igor.fcouto@gmail.com>
2022-06-07 01:20:05 -0300

    fix: reorganize and fix a typo in global usings file

--- GlobalUsing.cs
+++ GlobalUsing.cs
@@ -3,4 +3,5 @@ global using FluentValidation.AspNetCore;
 global using PetitAmourAPI.Configuration;
 global using PetitAmourAPI.Database;
 global using PetitAmourAPI.Database.Repositories;
-global using AccountingLedgerApi.Endpoints;
+global using PetitAmourAPI.Domain.Services;
+global using PetitAmourAPI.Endpoints;
