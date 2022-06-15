9fc7f7e2a1d4117ce9f5b595674de267dc4eabbc
Igor Couto <igor.fcouto@gmail.com>
2022-06-14 22:16:45 -0300

    Change to file scoped namespace

--- Domain/Validators/DateRequestValidator.cs
+++ Domain/Validators/DateRequestValidator.cs
@@ -1,16 +1,15 @@
 using FluentValidation;
 
-namespace PetitAmourAPI.Domain.Validators
-{
-    public class DateRequestValidator : AbstractValidator<DateRequest>
-    {
-        public DateRequestValidator()
-        {
-            var currentDate = DateTime.Now;
+namespace PetitAmourAPI.Domain.Validators;
 
-            RuleFor(x => x.Date)
-                .NotEmpty()
-                .GreaterThanOrEqualTo(currentDate);
-        }
+public class DateRequestValidator : AbstractValidator<DateRequest>
+{
+    public DateRequestValidator()
+    {
+        var currentDate = DateTime.Now;
+
+        RuleFor(x => x.Date)
+            .NotEmpty()
+            .GreaterThanOrEqualTo(currentDate);
     }
 }
