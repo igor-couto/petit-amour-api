7020ba665cc7a5e3ddf23702f3c4f4792d28c4c9
Igor Couto <igor.fcouto@gmail.com>
2022-06-14 22:16:26 -0300

    Change to file scoped namespace

--- Domain/Validators/OrderRequestValidator.cs
+++ Domain/Validators/OrderRequestValidator.cs
@@ -1,34 +1,33 @@
 using FluentValidation;
 
-namespace PetitAmourAPI.Domain.Validators
+namespace PetitAmourAPI.Domain.Validators;
+
+public class OrderRequestValidator : AbstractValidator<OrderRequest>
 {
-    public class OrderRequestValidator : AbstractValidator<OrderRequest>
+    public OrderRequestValidator()
     {
-        public OrderRequestValidator()
-        {
-            var currentDate = DateTime.Now;
+        var currentDate = DateTime.Now;
 
-            RuleFor(x => x.CustomerName)
-                .NotEmpty()
-                .MaximumLength(256);
+        RuleFor(x => x.CustomerName)
+            .NotEmpty()
+            .MaximumLength(256);
 
-            RuleFor(x => x.CustomerPhoneNumber)
-                .NotEmpty()
-                .MaximumLength(16);
+        RuleFor(x => x.CustomerPhoneNumber)
+            .NotEmpty()
+            .MaximumLength(16);
 
-            RuleFor(x => x.PaymentMethod)
-                .InclusiveBetween((short)0, (short)3);
+        RuleFor(x => x.PaymentMethod)
+            .InclusiveBetween((short)0, (short)3);
 
-            RuleFor(x => x.DeliveryDate)
-                .NotEmpty()
-                .GreaterThanOrEqualTo(currentDate);
+        RuleFor(x => x.DeliveryDate)
+            .NotEmpty()
+            .GreaterThanOrEqualTo(currentDate);
 
-            RuleFor(x => x.DeliveryAddress)
-                .NotEmpty()
-                .MaximumLength(512);
+        RuleFor(x => x.DeliveryAddress)
+            .NotEmpty()
+            .MaximumLength(512);
 
-            RuleFor(x => x.productRequest)
-                .NotEmpty();
-        }
+        RuleFor(x => x.productRequest)
+            .NotEmpty();
     }
 }
